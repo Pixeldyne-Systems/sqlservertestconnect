2bf56b94cec0bdaa2fcd2527526100d4598ce9df
Mac Plewa <Maciej.Plewa@velrada.com>
2020-04-15 08:50:43 +0800

    initial commit of working example

---
+++ src/SqlServerTestConnect/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace SqlServerTestConnect
+{
+	class Program
+	{
+        static readonly string query = "SELECT name FROM sys.databases";
+
+        static void Main(string[] args)
+		{
+            SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["test"].ConnectionString);
+
+            try
+            {
+                HasRows(sqlConnection);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        static void HasRows(SqlConnection connection)
+        {
+            using (connection)
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    Console.WriteLine("Connection successful...");
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("{0}", reader.GetString(0));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found, security issue?");
+                }
+                reader.Close();
+            }
+        }
+    }
+}
